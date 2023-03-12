5936d4326c296abac8678f769c97e4a68eddc58e
Bartłomiej Bujnarowski <bartlomiej.bujnarows@microsoft.wsei.edu.pl>
2023-03-12 11:16:24 +0100

    Create Type property, Started creating new class TODO later

---
+++ GradeBook/GradeBooks/StandardGradeBook .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GradeBook.GradeBooks
+{
+    internal class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) :base(name)
+        {
+            BaseGradeBook.Type = Enums.GradeBookType.Standard;
+           
+        }
+    }
+}
